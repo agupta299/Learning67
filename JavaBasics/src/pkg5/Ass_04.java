7a0320022a296e3745622a4cf999efe01424b5ea
apgupta <apgupta@LAP_ApurvaGupta.q3infotech.net>
2026-04-15 14:36:12 +0530

    Added both Ass_04 and ass_05

---
+++ JavaBasics/src/pkg5/Ass_04.java
@@ -0,0 +1,17 @@
+package pkg5;
+
+public class Ass_04 {
+
+
+	public static void main(String[] args) {
+		int a =100;
+		int b= 20;
+System.out.println("Addition is = "  + (a+b));
+System.out.println("Subtraction is = "  + (a-b));
+System.out.println("Multiplication is = "  + (a*b));
+System.out.println("Division is = "  + (a/b));
+System.out.println("Mode is = "+  (a%b));
+		
+	}
+
+}
